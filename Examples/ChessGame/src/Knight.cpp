dcd99b3bbea63d0ae8e3b7acf19202e94b3e2659
Mariyan Kilimov <mariqnkilimov18d@gmail.com>
2026-05-05 01:14:21 +0300

    Added logic for bishop, knight and queen chess pieces!

--- Examples/ChessGame/src/Knight.cpp
+++ Examples/ChessGame/src/Knight.cpp
@@ -7,16 +7,86 @@ Knight::Knight(FigureColor Color) : Figure(Color) {
 std::vector<MovePoint> Knight::move(int x, int y, Figure* board[8][8]) {
 	std::vector<MovePoint> moves;
 
-	int dir = (getColor() == WHITE) ? 1 : -1;
 
-	if (!board[y + dir][x])
-		moves.push_back({ x, y + dir });
+	// up 2 - right 1
+	if (y + 2 < 8 && x + 1 < 8)
+		if (board[y + 2][x + 1]) {
+			if (board[y + 2][x + 1]->getColor() != getColor())
+				moves.push_back({ x + 1, y + 2 });
+		} 
+		else {
+			moves.push_back({ x + 1, y + 2 });
+		}
 
-	if (board[y + dir][x + 1])
-		moves.push_back({ x + 1, y + dir });
+	// up 1 - right 2
+	if (y + 1 < 8 && x + 2 < 8)
+		if (board[y + 1][x + 2]) {
+			if (board[y + 1][x + 2]->getColor() != getColor())
+				moves.push_back({ x + 2, y + 1 });
+		}
+		else {
+			moves.push_back({ x + 2, y + 1 });
+		}
 
-	if (board[y + dir][x - 1])
-		moves.push_back({ x - 1, y + dir });
+	// down 2 - left 1
+	if (y - 2 >= 0 && x - 1 >= 0)
+		if (board[y - 2][x - 1]) {
+			if (board[y - 2][x - 1]->getColor() != getColor())
+				moves.push_back({ x - 1, y - 2 });
+		}
+		else {
+			moves.push_back({ x - 1, y - 2 });
+		}
+
+	// down 1 - left 2
+	if (y - 1 >= 0 && x - 2 >= 0)
+		if (board[y - 1][x - 2]) {
+			if (board[y - 1][x - 2]->getColor() != getColor())
+				moves.push_back({ x - 2, y - 1 });
+		}
+		else {
+			moves.push_back({ x - 2, y - 1 });
+		}
+
+	// up 2 - left 1
+	if (y + 2 < 8 && x - 1 >= 0)
+		if (board[y + 2][x - 1]) {
+			if (board[y + 2][x - 1]->getColor() != getColor())
+				moves.push_back({ x - 1, y + 2 });
+		}
+		else {
+			moves.push_back({ x - 1, y + 2 });
+		}
+
+	// up 1 - left 2
+	if (y + 1 < 8 && x - 2 >= 0)
+		if (board[y + 1][x - 2]) {
+			if (board[y + 1][x - 2]->getColor() != getColor())
+				moves.push_back({ x - 2, y + 1 });
+		}
+		else {
+			moves.push_back({ x - 2, y + 1 });
+		}
+
+	// down 2 - right 1
+	if (y - 2 >= 0 && x + 1 < 8)
+		if (board[y - 2][x + 1]) {
+			if (board[y - 2][x + 1]->getColor() != getColor())
+				moves.push_back({ x + 1, y - 2 });
+		}
+		else {
+			moves.push_back({ x + 1, y - 2 });
+		}
+
+	// down 1 - right 2
+	if (y - 1 >= 0 && x + 2 < 8)
+		if (board[y - 1][x + 2]) {
+			if (board[y - 1][x + 2]->getColor() != getColor())
+				moves.push_back({ x + 2, y - 1 });
+		}
+		else {
+			moves.push_back({ x + 2, y - 1 });
+		}
 
 	return moves;
 }
